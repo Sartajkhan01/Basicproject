1a1dc04f3de77a66bce8e960b3eb2e46b9747441
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-14 23:23:40 +0530

    Add files via upload

---
+++ Basicproject/MultipleCompanies.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicproject
+{
+    internal class MultipleCompanies
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem on Master Branch");
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            Random random = new Random();
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                int empCheck = random.Next(0, 3);
+                totalWorkingDays++;
+                switch (empCheck)
+                {
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:" + totalWorkingDays + " Emp hrs " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage("DMart", 20, 2, 10);
+            computeEmpWage("Reliance", 10, 4, 20);
+        }
+    }
+}
