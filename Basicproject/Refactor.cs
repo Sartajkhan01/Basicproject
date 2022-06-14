b91cfd59eebbb0f96936a33dffe519932a7c2d7c
Sartajkhan01 <106169841+Sartajkhan01@users.noreply.github.com>
2022-06-14 23:25:52 +0530

    Add files via upload

---
+++ Basicproject/Refactor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicproject
+{
+    internal class Refactor
+    {
+        public int total_Emp_Wage;
+        public int empWageperHr;
+        public string company;
+        public int empWagePerHr;
+        public int workingDaysPerMonth;
+        public int maxHrsPerMonth;
+        internal int empRatePerHour;
+        internal int nofoWorkingDays;
+        private int empWagePerHour;
+        private int maxHoursPerMonth;
+
+        public void CompanyEmpWage(string company, int empWagePerHour, int workingDaysPerMonth, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empWagePerHour = empWagePerHour;
+            this.workingDaysPerMonth = workingDaysPerMonth;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public void CompanyEmpWage(string company, int empWagePerHr, int workingDaysPerMonth, int maxHrsPerMonth, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empWagePerHr = empWagePerHr;
+            this.workingDaysPerMonth = workingDaysPerMonth;
+            this.maxHrsPerMonth = maxHrsPerMonth;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.total_Emp_Wage = totalEmpWage;
+        }
+
+        public string tostring()
+        {
+            return "Total Emp Wage for company : " + this.company + " is: " + this.total_Emp_Wage;
+        }
+
+    }
+}
